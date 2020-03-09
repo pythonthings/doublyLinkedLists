1b5b272151e520112bf3fefcf61d5f02941968bb
Roinzunza <rosendoinzunza@.com>
2020-03-09 11:11:55 -0700

    implemented insert_after function

--- DoublyLists.py
+++ DoublyLists.py
@@ -3,6 +3,7 @@ This is a Doubly Linked List in python
 Functions
 - push: adds to front of the list
 - print_list: displays list in order and in revesed order
+- insert_after: point new node to previous node and the node after the previous with previous pointer and next pointers
 '''
 class Node:
     def __init__(self,data):
@@ -22,6 +23,21 @@ class Lists:
             self.head.prev = new_node
         self.head = new_node
 
+    def insert_after(self,prev_node,data):
+        if prev_node is None:
+            print('No previous node')
+            return
+        new_node = Node(data)
+        #point next of new_node to the next node of previous
+        new_node.next = prev_node.next
+        #point previous node to the new node
+        prev_node.next = new_node
+        #point new node previous to previous node
+        new_node.prev = prev_node
+        if new_node.next is not None:
+            #point the node after the new nodes previous pointer back to the new node
+            new_node.next.prev = new_node
+
     def print_list(self):
         node = self.head
         print('In order: ')
@@ -40,4 +56,8 @@ class Lists:
 list = Lists()
 list.push(6)
 list.push(3)
+list.push(3)
+list.push(3)
+list.push(3)
+list.insert_after(list.head.next,55)
 list.print_list()
